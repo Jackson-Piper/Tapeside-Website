682df0dad8cbaa87aa03e73ac436c2fd39ebdcd5
Voidoz <41652531+Voidoz@users.noreply.github.com>
2021-04-09 00:56:36 +1000

    made endnote inline

--- src/components/Endnote.jsx
+++ src/components/Endnote.jsx
@@ -4,26 +4,25 @@ import { OverlayTrigger, Tooltip, Button } from "react-bootstrap";
 export default class Endnote extends React.Component {
   render() {
     return (
-      <div>
-        <OverlayTrigger
-          key={this.props.placement}
-          placement={this.props.placement}
-          overlay={
-            <Tooltip id={`tooltip-${this.props.placement}`}>
-              {this.props.note}
-            </Tooltip>
-          }
+      <OverlayTrigger
+        key={this.props.placement}
+        placement={this.props.placement}
+        overlay={
+          <Tooltip id={`tooltip-${this.props.placement}`}>
+            {this.props.note}
+          </Tooltip>
+        }
+        style={{display: "inline"}}
+      >
+        <Button
+          disabled={this.props.disabled}
+          href={this.props.link}
+          target="_blank"
+          id="endnote-button"
         >
-          <Button
-            disabled={this.props.disabled}
-            href={this.props.link}
-            target="_blank"
-            id="endnote-button"
-          >
-            <sup>[{this.props.tooltip}]</sup>
-          </Button>
-        </OverlayTrigger>
-      </div>
+          <sup>[{this.props.tooltip}]</sup>
+        </Button>
+      </OverlayTrigger>
     );
   }
 }
