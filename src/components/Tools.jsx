7e23e40f4ef8cf1cb5d91ec19105a0207335b4ec
Voidoz <41652531+Voidoz@users.noreply.github.com>
2021-04-20 12:06:27 +1000

    fixed code formatting on tools page

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -5,10 +5,10 @@ export default class Tools extends React.Component {
   render() {
     return (
       <div>
-      <ContentCard align="center">
-        <h2>Tools</h2>
-      </ContentCard>
-    </div>
+        <ContentCard align="center">
+          <h2>Tools</h2>
+        </ContentCard>
+      </div>
     );
   }
 }
