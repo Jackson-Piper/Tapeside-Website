6b05e398641d13cf09f04b858ae441cab030fc95
Voidoz <41652531+Voidoz@users.noreply.github.com>
2021-04-20 11:58:27 +1000

    Added content to machine learning page

--- src/components/Tech/MachineLearning.jsx
+++ src/components/Tech/MachineLearning.jsx
@@ -10,7 +10,196 @@ export default class MachineLearning extends React.Component {
           <h2>Machine Learning</h2>
         </ContentCard>
         <ContentCard>
-          <p></p>
+          <p>
+            <strong>What is it?</strong>
+            <br />
+            <br />
+            Machine learning is the process in which a machine learns to make
+            predictions based on a series of data presented to it. This machine
+            will then learn to make correlations between data it already has,
+            and the new data that it must evaluate and label. A worked example
+            of this would be using machine learning to determine weather a drink
+            was beer or wine based on its ABV% and color. Other real-life
+            examples of machine learning include fraud detection for banks and
+            assisting doctors in the healthcare sector with diagnostics. This
+            seemingly futuristic technology is happening all around you, all the
+            time every second of your life, somewhere some unit is using your
+            information to make assumptions and alter its systems to “best” suit
+            you.
+            <br />
+            <br />
+            <br />
+            <strong>Why now</strong>
+            <br />
+            <br />
+            Machine learning has seen a huge jump in popularity and efficiency
+            in recent years for several reasons. While the term was coined in
+            1959 by Arthur Samuel, it was not until 2010 that it really started
+            to take off with the increase of quantity and quality of data being
+            able to be provided as everything started to move online and this
+            information started to be collected, stored, and used, the ethical
+            dilemma of this data collection is still under debate. The computing
+            and processing power available now also heavily outclasses all that
+            have come before which allows this vast sum of data to have a use,
+            parallel to processing power our memory storage capabilities have
+            also increased again allowing this much needed data to be stored.
+            <br />
+            <br />
+            <br />
+            <strong>Future (3 Years)</strong>
+            <br />
+            <br />
+            There are a lot of buzz words floating around when it comes to
+            machine learning and the biggest of them is obviously Artificial
+            Intelligence (A.I). While we are years and years of a proper
+            functioning A.I there are a few of these sci-fi concepts are
+            becoming a reality. The biggest will be the implementation of
+            quantum computer into machine learning
+            <Endnote
+              tooltip="1"
+              placement="bottom"
+              note="Ahmed, A., 2021. What is the Future of Machine Learning"
+              link="https://medium.com/machine-learning-quest/what-is-the-future-of-machine-learning-ec17bfd3b90b"
+            />
+            . This will vastly accelerate the overall processing, learning and
+            increase the capabilities of these systems. Millions and millions
+            have been invested into quantum computing machine learning as it is
+            clearly the future for many fields including healthcare and medicine
+            <Endnote
+              tooltip="2"
+              placement="bottom"
+              note="Neo, B., 2021. The future of Machine Learning."
+              link="https://towardsdatascience.com/the-future-of-machine-learning-ce0a9dc18cb8"
+            />
+            .
+            <br />
+            <br />
+            <br />
+            <strong>Why aren’t we there yet?</strong>
+            <br />
+            <br />
+            While we have come a long way forward with computing in the last
+            couple of decades, we still have many hurdles that we are yet to
+            overcome. The biggest is creating a Machine Learning model that does
+            not require starting from scratch every time you need to change the
+            unit’s task
+            <Endnote
+              tooltip="2"
+              placement="bottom"
+              note="Neo, B., 2021. The future of Machine Learning."
+              link="https://towardsdatascience.com/the-future-of-machine-learning-ce0a9dc18cb8"
+            />
+            . The inefficiency of both effort in and computationally complexity
+            of these models is what is holding back the large advancements
+            <Endnote
+              tooltip="2"
+              placement="bottom"
+              note="Neo, B., 2021. The future of Machine Learning."
+              link="https://towardsdatascience.com/the-future-of-machine-learning-ce0a9dc18cb8"
+            />
+            . The next step or leap as it will probably be known will come when
+            a multifunctioning or multitasking model that can do a multitude of
+            different tasks is created according to Jeff Dean a Google Senior
+            Fellow
+            <Endnote
+              tooltip="2"
+              placement="bottom"
+              note="Neo, B., 2021. The future of Machine Learning."
+              link="https://towardsdatascience.com/the-future-of-machine-learning-ce0a9dc18cb8"
+            />
+            . This steppingstone between a machine learning to do one task and
+            an almost Artificial Intelligence knowing “how to route the
+            different pieces of the model that is most appropriate.”
+            <br />
+            <br />
+            <br />
+            <strong>Likely Impact</strong>
+            <br />
+            <br />
+            If you were to investigate the future a few years not much would
+            appear to have changed. However underneath the apparent normalcy of
+            everyday life there will be hundreds of thousands of different
+            personalized devices. The potential impact of algorithms or systems
+            that can shift through huge sums of data and come out with
+            remarkable accurate results is already being felt all over the
+            world.
+            <br />
+            <br />
+            Already we are seeing smart watches dictate the training schedule of
+            scores of people based on the information they have gathered or been
+            provided by the user. While everyday life may not be impacted to
+            heavily robots will start to become more and more common place
+            within the business environment. Already we are seeing basic
+            warehouse tasks getting given to simple robots or drones and this
+            will only increase as the efficiency and “intelligence” of these
+            robots increases
+            <Endnote
+              tooltip="4"
+              placement="bottom"
+              note="Pickell, D., 2021. 5 IoT Devices That Could Change Our Lives in the Very Near Future."
+              link="https://learn.g2.com/iot-devices"
+            />
+            .
+            <br />
+            <br />
+            Another version of these “robots” that will affect the large
+            majority of the population is the rise of the “Self-Driving Car”.
+            While commercial planes have used GPS, motion sensor and other
+            systems to make pilots almost redundant after takeoff. Car companies
+            are now employing similar systems to allow for these cars to drive
+            themselves
+            <Endnote
+              tooltip="4"
+              placement="bottom"
+              note="Pickell, D., 2021. 5 IoT Devices That Could Change Our Lives in the Very Near Future."
+              link="https://learn.g2.com/iot-devices"
+            />
+            <Endnote
+              tooltip="5"
+              placement="bottom"
+              note="Medium. 2021. MACHINE LEARNING AND IT’S IMPACT ON SOCIETY."
+              link="https://medium.com/@escapadetech/machine-learning-and-its-impact-on-society-5c92eb9f5cc3"
+            />
+            . This self-driving system will soon become more and more common
+            with each new iteration learning and improving from the last.
+            <br />
+            <br />
+            While there are many positives coming out of these systems there are
+            also many ethical dilemmas that also arise. Systems such as facial
+            recognition which has been heavily implemented all throughout China
+            which raises many ethical questions as to one’s privacy in this
+            technological age. The rise of self-driving questions also begs the
+            question what happens to the truck drivers and other couriers when
+            they are replaced by these autonomous systems. The redundancy and
+            replacement of low skilled manual labor will cause waves throughout
+            all countries in the worlds as more and more companies will find it
+            easier to replace their staff with robots
+            <Endnote
+              tooltip="5"
+              placement="bottom"
+              note="Medium. 2021. MACHINE LEARNING AND IT’S IMPACT ON SOCIETY."
+              link="https://medium.com/@escapadetech/machine-learning-and-its-impact-on-society-5c92eb9f5cc3"
+            />
+            .
+            <br />
+            <br />
+            <br />
+            <strong>How it will affect you</strong>
+            <br />
+            <br />
+            The affects of machine learning can already be felt in anyone’s day
+            to day life. Just as the personalized computer and smart phone
+            slowly crept into every household so has machine learning. The adds
+            you see on your social media platforms are not there by accident,
+            they have been chosen based on all the information collected from
+            you and tailored to you recent history across all of your devices.
+            While no immediate changes will really be felt, slowly as the
+            healthcare models improve a better and faster quality of care and
+            life will begin to creep in, as will the models predicting
+            environmental changes both in weather and climate change. All the
+            personal affects each “average joe” will experience will happen in
+            such minute amounts that you wont even notice it as it happens.
+          </p>
         </ContentCard>
       </div>
     );
