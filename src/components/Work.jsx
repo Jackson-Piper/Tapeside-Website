235cf0509208e18aa2ee7a393993972975f543da
Voidoz <41652531+Voidoz@users.noreply.github.com>
2021-04-22 17:39:08 +1000

    added video

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,6 +8,17 @@ export default class Work extends React.Component {
         <ContentCard align="center">
           <h2>IT Work</h2>
         </ContentCard>
+        <ContentCard align="center">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/bSXDW07oKZQ"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </ContentCard>
       </div>
     );
   }
