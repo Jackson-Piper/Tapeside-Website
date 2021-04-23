71e32ea8dd27e50f317f924628f0eee40373f005
Voidoz <41652531+Voidoz@users.noreply.github.com>
2021-04-23 11:18:12 +1000

    filled out the remaining fields on the project

--- src/components/Project/Motivation.jsx
+++ src/components/Project/Motivation.jsx
@@ -7,7 +7,50 @@ export default class Motivation extends React.Component {
       <ContentCard>
         <h1>Motivation</h1>
         <br />
-        <p></p>
+        <p>
+          Intensively searching for a solution to easily and properly manage
+          audio sources and devices have been extremely difficult and
+          frustrating, as most solutions required either expensive audio
+          interfaces or bulky mixers alongside the plethora of cables required.
+          There are indeed software-based solutions, however the majority that
+          are available require either previous knowledge of audio
+          mixing/interfaces, non-beginner-friendly and/or cumbersome which is
+          the case of the VB-Audio suite.
+          <br />
+          <br />
+          With the peak and current situation of the pandemic which forcibly
+          confined everyone within their homes, streaming content either it be
+          games, movies, shows, etc. popular platforms –Twitch, YouTube,
+          Facebook, and other streaming platforms have bloomed drastically with
+          Twitch gaining roughly 5.7 million new creators and other platforms
+          underwent the increase of new creators. Growth fulfilled the
+          satisfaction of the ever-growing hunger of content by people and a way
+          to pass time of lockdown.
+          <br />
+          <br />
+          New content creators, especially those who stream will mostly likely
+          have to spend time routing their audio through OBS or similar programs
+          for their streams. This can be daunting to certain creators due to the
+          forementioned reasons above to obtain their desired results, thus
+          resulting in TapeSide Technologies creating a software-based audio
+          interface/mixer that is both user and beginner friendly.
+          <br />
+          <br />
+          This program enables the user to properly, cleanly, and easily adjust
+          and control audio of different sources and devices ranging from game
+          and chat audio to physical interfaces regarding the user’s skill
+          set(s). This approach allows recent/new creators or even individuals
+          that require such program to easily control and route audio channels
+          to different outputs through the clean and easy to understand
+          user-interface.
+          <br />
+          <br />
+          Simple and user friendly experience of the software enables non-tech
+          savvy people to seamlessly and easily fully control their audio and
+          devices to their desire with the limit being their knowledge, and with
+          the current alternative programs available only a couple are somewhat
+          close to what DigiWave is able to provide to new users.
+        </p>
       </ContentCard>
     );
   }
