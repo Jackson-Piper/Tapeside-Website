116556aebedc32c6ce1f641ca61f2faf069b203c
Voidoz <41652531+Voidoz@users.noreply.github.com>
2021-04-23 22:48:40 +1000

    added Robotics page content

--- src/components/Tech/Robotics.jsx
+++ src/components/Tech/Robotics.jsx
@@ -10,7 +10,175 @@ export default class Robotics extends React.Component {
           <h2>Robotics</h2>
         </ContentCard>
         <ContentCard>
-          <p></p>
+          <p>
+            Robotics is the design, construction, and use of machines to aid or
+            complete task which are traditionally done by humans
+            <Endnote
+              tooltip="i"
+              placement="bottom"
+              note="Britannica (2019). Robotics | technology. In: Encyclopædia Britannica."
+              link="https://www.britannica.com/technology/robotics"
+            />
+            . It is the intersection of science, engineering, and technology
+            alongside decades of innovations and Research and Development that
+            allows production of machines (robots)
+            <Endnote
+              tooltip="ii"
+              placement="bottom"
+              note="Builtin (2019). What Is Robotics? Types Of Robots | Built In."
+              link="https://builtin.com/robotics"
+            />
+            . As technology continue to advance and evolve, the field of
+            robotics will continue to advance, evolve, and expand as well into
+            different fields of technology. Currently robotics is mainly
+            utilized in 5 different industries – Health Care, Agriculture, Food
+            Preparation, Manufacturing, and Military
+            <Endnote
+              tooltip="iii"
+              placement="bottom"
+              note="OHIO University (2018). Top 5 Industries Utilizing Robotics."
+              link="https://onlinemasters.ohio.edu/blog/5-industries-utilizing-robotics/#:~:text=Robotics%20are%20being%20used%20in"
+            />
+            .Each industry has its unique utilization of the technology albeit
+            being an extremely precise surgical robot which enables
+            professionals to perform surgery inside the body; exoskeleton to
+            assist rehabilitation to paralysed patients; an intelligent and
+            automated robot with the ability to cook predetermined meals; bomb
+            detecting, and defusing robots used in past/current warfare; aiding
+            in the creation and manufacturing of automobiles.
+            <br />
+            <br />
+            Healthcare robotics appear in a plethora of areas that directly
+            affect the patient’s care, through disinfecting patient rooms and
+            operating suites, reducing risks for patients and medical personnel.
+            They are also utilized in the traditional manner being in
+            laboratories, performing task such as transportation, analysation,
+            and storying of samples
+            <Endnote
+              tooltip="iv"
+              placement="bottom"
+              note="Healthcare Administration Degree (n.d.). How are Robots Changing Healthcare?"
+              link="https://www.healthcare-administration-degree.net/faq/how-are-robots-changing-healthcare/#:~:text=Robotic%20technologies%20appear%20in%20many"
+            />
+            . Major and critical application of robotic is through
+            robot-assisted surgery, allowing doctors to perform delicate nano
+            surgeries with the combination of professional skills and adaptive
+            learning of artificial intelligence resulting in immense precision,
+            flexibility, and control than currently possible with conventional
+            techniques withing surgeries
+            <Endnote
+              tooltip="v"
+              placement="bottom"
+              note="Mayo Clinic (2019). Robotic surgery - Mayo Clinic."
+              link="https://www.mayoclinic.org/tests-procedures/robotic-surgery/about/pac-20394974"
+            />
+            .
+            <br />
+            <br />
+            As the pressure on farmers increase due to the surge of the global
+            population and the shortage of labourers, famers have integrated
+            numerous technologies to combat the prevailing issues and improve
+            efficiency and reduce operation cost within their farm. Some of the
+            technologies that have been integrated are mostly autonomous
+            robotics ranging in size serve important task such as monitoring the
+            health and development of crops on a basis; transportation and
+            organisation of inventory; Multi-purpose robot capable of planting
+            seeds, watering individual plants; measurement of moisture content
+            of the soil; removal of weeds and measure the health of the farm
+            <Endnote
+              tooltip="vi"
+              placement="bottom"
+              note="Farmmachinerysales (2019). 10 robots changing the way we farm."
+              link="https://www.farmmachinerysales.com.au/editorial/details/10-robots-changing-the-way-we-farm-117873/#:~:text=A%20low%2Dcost%20crop%20robot"
+            />
+            . Implementations of robotics on farms allows farmers to somewhat
+            relax knowing their farm is working efficiently and properly.
+            <br />
+            <br />
+            Military have implemented robotics in warfare for years now as an
+            alternative to human soldiers and have been designed to perform a
+            broad range of combat specific task, from eliminating opposing
+            infantry to target acquisition with frightening efficiency and being
+            an alternative to human soldiers they can be deployed in dangerous
+            or harsh situations
+            <Endnote
+              tooltip="vii"
+              placement="bottom"
+              note="Kumari, A.M. and S. (2018). Military Robots Play a Pivotal Role as a Tactical and Operational Tool for Armed Forces."
+              link="https://blog.marketresearch.com/military-robots-play-a-pivotal-role-as-a-tactical-and-operational-tool-for-armed-forces#:~:text=Robots%20in%20the%20military%20are%20deployed%20in%20combat%20support%20application"
+            />
+            . Most notable robots that are used within the military is the
+            ever-famous Unmanned Aerial Vehicle (UAV) used for surveillance
+            missions; remote-controlled mine clearing robots; search and rescue
+            bots; and plethora more known and classified robotic technology
+            <Endnote
+              tooltip="viii"
+              placement="bottom"
+              note="Joseph (2019). Military Robots: What Are They?"
+              link="https://robots.net/robotics/military-robots/"
+            />
+            . Robotic technology within the military saved and spared the lives
+            of many soldiers from harm’s way.
+            <br />
+            <br />
+            With technology advancing greatly each year, the possibilities and
+            usability of robotics will skyrocket as well as its availability
+            with it becoming more readily available and affordable to the
+            consumer market. This allows education institutes to teach
+            younglings of the proper use and implantation of technology as
+            society and technology begin to merge greater.
+            <br />
+            <br />
+            The impact of robotics to society is mixed feelings due to the
+            opportunities that it provides and opportunities that decline
+            individuals and the concerns of people from relying on technology.
+            Mass implementation robotic technology within the industries
+            immensely provides society a productivity growth, resulting in
+            higher living standards and wages lower pricing of goods and
+            services, and an overall greater variety of products and services.
+            High-paying opportunities are also created due to the requirements
+            of skilled personal as robotics and its complex and intricate
+            maintenance and operation is crucial
+            <Endnote
+              tooltip="ix"
+              placement="bottom"
+              note="Anthony, C. (2019). 3 Ways Robots Affect the Economy."
+              link="https://www.investopedia.com/articles/markets-economy/091316/3-ways-robots-affect-economy.asp#:~:text=While%20there%20may%20be%20a"
+            />
+            .
+            <br />
+            <br />
+            It is apparent that low-skilled workers will be replaced and their
+            task automated, causing loss of employment and potentially cause; as
+            technology advances further the range of potential application and
+            roles that robotics can be utilized and occupy will only expand,
+            specialized fields and professions such as medical diagnostics and
+            even surgical care providers will be affected by the widespread of
+            robotics
+            <Endnote
+              tooltip="x"
+              placement="bottom"
+              note="DelGado, R. (2017). The Negative Effects of Robots Entering the Workforce."
+              link="https://www.itbriefcase.net/the-negative-effects-of-robots-entering-the-workforce"
+            />
+            . Converting from a robotic workforce would entail in hefty expenses
+            in the beginning this, and the overall cost to automate and
+            environment will no doubt create obstacles and problems to
+            organizations with limited financial resources.
+            <br />
+            <br />
+            The personal affects that automation possess on my daily life will
+            be either by minimal or adequate as depending on the career that I
+            pursue will greatly determine how this technology affects me. A
+            career like programming revolves my entire work effort into coding
+            and software development for these machines/robots, whereas anything
+            outside technology will be minimal as during years leading to
+            retirement, I believe robotics would have advanced enough to cover
+            every work force as certain jobs/work is far greater done manually
+            instead of automatically. Certain family members might be affected
+            by the technology to the degree that they’ll require to learn to
+            work with the technology.
+          </p>
         </ContentCard>
       </div>
     );
